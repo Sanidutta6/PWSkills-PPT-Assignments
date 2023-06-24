3a0a7dcc245b764cf149bad94e2c0e611699873c
Suny Dutta <sanidutta6@gmail.com>
2023-06-24 11:23:27 +0530

    Updated

--- DSA - Module_1/Assignments - 1/question-3.js
+++ DSA - Module_1/Assignments - 1/question-3.js
@@ -1,5 +1,6 @@
 /**
-  Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+  Given a sorted array of distinct integers and a target value, return the index if the target is found.
+  If not, return the index where it would be if it were inserted in order.
   You must write an algorithm with O(log n) runtime complexity.
  */
 
@@ -19,8 +20,8 @@ const BinarySearch = (nums, val) => {
     }
   }
 
-  return -1;
+  return low;
 };
 
-const nums = [1,3,5,6], target = 6;
+const nums = [1,3,5,6], target = 4;
 console.log(BinarySearch(nums, target));
