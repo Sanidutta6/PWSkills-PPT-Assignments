badacd337caa9ddb9ae0966450dfb503a83c9a3d
Suny Dutta <sanidutta6@gmail.com>
2023-06-24 11:22:59 +0530

    Updated

--- DSA - Module_1/Assignments - 2/question-6.js
+++ DSA - Module_1/Assignments - 2/question-6.js
@@ -10,3 +10,25 @@ Output: 4
 
 Explanation: 9 exists in nums and its index is 4
 */
+
+const BinarySearch = (nums, val) => {
+    // Non-recursive approach
+    let low = 0, high = nums.length - 1;
+  
+    while (low <= high) {
+      let mid = Math.floor((low + high) / 2);
+  
+      if (nums[mid] === val) {
+        return mid;
+      } else if (nums[mid] < val) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
+    }
+  
+    return -1;
+  };
+  
+  const nums = [-1,0,3,5,9,12], target = 9;
+  console.log(BinarySearch(nums, target));
