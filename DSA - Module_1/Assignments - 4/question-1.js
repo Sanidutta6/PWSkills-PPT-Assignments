5eaea756d66c6609f88c768a07df060827288c95
Suny Dutta <sanidutta6@gmail.com>
2023-06-25 19:15:29 +0530

    Updated

--- DSA - Module_1/Assignments - 4/question-1.js
+++ DSA - Module_1/Assignments - 4/question-1.js
@@ -9,3 +9,30 @@ Output: [1,5]
 
 **Explanation:** Only 1 and 5 appeared in the three arrays.
 */
+
+const findCommonElements = (arr1, arr2, arr3) => {
+  let result = [];
+  let i = 0,
+    j = 0,
+    k = 0;
+
+  while (i < arr1.length && j < arr2.length && k < arr3.length) {
+    if (arr1[i] === arr2[j] && arr2[j] === arr3[k]) {
+      result.push(arr1[i]);
+      i++;
+      j++;
+      k++;
+    } else if (arr1[i] < arr2[j]) {
+      i++;
+    } else if (arr2[j] < arr3[k]) {
+      j++;
+    } else {
+      k++;
+    }
+  }
+
+  return result;
+};
+
+const arr1 = [1,2,3,4,5], arr2 = [1,2,5,7,9], arr3 = [1,3,4,5,8];
+console.log(findCommonElements(arr1, arr2, arr3));
--- DSA - Module_1/Assignments - 4/question-3.js
+++ DSA - Module_1/Assignments - 4/question-3.js
@@ -9,3 +9,22 @@ Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
 
 Output: [[1,4,7],[2,5,8],[3,6,9]]
 */
+
+const transposeMatrix = (matrix) => {
+  const transpose = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    const temp = [];
+    for (let j = 0; j < matrix.length; j++) {
+      temp.push(matrix[j][i]);
+    }
+    transpose.push(temp);
+  }
+  return transpose;
+};
+
+const matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+console.log(transposeMatrix(matrix));
