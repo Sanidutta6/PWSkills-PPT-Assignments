e01fa7ad614afe1b46f79c379d1463e68655fdbf
Suny Dutta <sanidutta6@gmail.com>
2023-06-27 07:12:25 +0530

    Updated

--- DSA - Module_1/Assignments - 7/question-4.js
+++ DSA - Module_1/Assignments - 7/question-4.js
@@ -7,3 +7,24 @@ Given a string s, reverse the order of characters in each word within a sentence
 
 **Output:** "s'teL ekat edoCteeL tsetnoc"
 */
+
+const reverseStr = (str) => {
+    let reversed = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+      reversed += str[i];
+    }
+    return reversed;
+  }
+
+const ReverseWordsInSentence = (sentence) => {
+    const words = sentence.split(" ");
+
+    for(let i = 0;i < words.length;i++) {
+        words[i] = reverseStr(words[i]);
+    }
+
+    return words.join(" ");
+}
+
+const s = "Let's take LeetCode contest";
+console.log(ReverseWordsInSentence(s));
