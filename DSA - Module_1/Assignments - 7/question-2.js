314e62f66f05ecaf6a8aebd4bc432d42d1c884f7
Suny Dutta <sanidutta6@gmail.com>
2023-06-27 09:06:01 +0530

    Updated

--- DSA - Module_1/Assignments - 7/question-2.js
+++ DSA - Module_1/Assignments - 7/question-2.js
@@ -11,3 +11,38 @@ A **strobogrammatic number** is a number that looks the same when rotated 180 de
 
 true
 */
+
+const IsStrobogrammatic = (num) => {
+  const strobogrammaticMap = {
+    0: "0",
+    1: "1",
+    6: "9",
+    8: "8",
+    9: "6",
+  };
+
+  let left = 0;
+  let right = num.length - 1;
+
+  while (left <= right) {
+    const leftChar = num[left];
+    const rightChar = num[right];
+
+    if (
+      !(
+        leftChar in strobogrammaticMap &&
+        strobogrammaticMap[leftChar] === rightChar
+      )
+    ) {
+      return false;
+    }
+
+    left++;
+    right--;
+  }
+
+  return true;
+};
+
+const num = "69";
+console.log(IsStrobogrammatic(num));
--- DSA - Module_1/Assignments - 7/question-3.js
+++ DSA - Module_1/Assignments - 7/question-3.js
@@ -11,3 +11,38 @@ You must solve the problem without using any built-in library for handling large
 
 "134"
 */
+
+const ParseDigit = (digit) => {
+    const MAP = {
+        "0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9
+    }
+
+    return MAP[digit];
+}
+
+const StrNumAddition = (num1, num2) => {
+  let result = "";
+  let carry = 0;
+  let i = num1.length - 1;
+  let j = num2.length - 1;
+
+  while (i >= 0 || j >= 0 || carry > 0) {
+    const digit1 = i >= 0 ? ParseDigit(num1[i]) : 0;
+    const digit2 = j >= 0 ? ParseDigit(num2[j]) : 0;
+
+    const sum = digit1 + digit2 + carry;
+    carry = Math.floor(sum / 10);
+    const digit = sum % 10;
+
+    result = digit.toString() + result;
+
+    i--;
+    j--;
+  }
+
+  return result;
+};
+
+const num1 = "11",
+  num2 = "123";
+console.log(StrNumAddition(num1, num2));
